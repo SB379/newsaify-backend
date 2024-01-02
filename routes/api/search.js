6a5dc3162627e81c244cdef3daa2ee8d1268fe7c
SB379 <sidbalaji03@gmail.com>
2024-01-02 18:33:39 -0500

    rollback to better syntax

--- routes/api/search.js
+++ routes/api/search.js
@@ -23,73 +23,26 @@ const openai = new OpenAI({
 
 router.get('/test', (req, res) => res.send("search route testing"));
 
-// router.get('/getArticles', async (req, res, next) => {
-
-//     // const authorizationHeader = req.headers.authorization || null;
-  
-//     // if (!authorizationHeader) {
-//     //   console.error('Access token is missing.');
-//     //   res.status(401).json({ error: 'Unauthorized' });
-//     //   return;
-//     // }
-
-//     // console.log(authorizationHeader);
-
-//     // const config = {
-//     //     headers: {
-//     //         'Authorization': authorizationHeader
-//     //     }
-//     // };
-
-//     try {
-
-//         const { q } = req.query;
-
-//         const response = await axios.get('https://customsearch.googleapis.com/customsearch/v1', {
-//             params: {
-//                 key: process.env.CUSTOM_SEARCH_KEY,
-//                 cx: process.env.ENGINE_KEY,
-//                 q,
-//                 start: 0,
-//                 num: 10,
-//             },
-//             // config
-//         });
-
-//         const { data } = response;
-//         const { queries, items, searchInformation } = data;
-
-//         const page = (queries.request || [])[0] || {};
-//         const previousPage = (queries.previousPage || [])[0] || {};
-//         const nextPage = (queries.nextPage || [])[0] || {};
-
-//         const resultData = {
-//             q,
-//             totalResults: page.totalResults,
-//             count: page.count,
-//             startIndex: page.startIndex,
-//             nextPage: nextPage.startIndex,
-//             previousPage: previousPage.startIndex,
-//             time: searchInformation.searchTime,
-//             items: items.map(o => ({
-//                 link: o.link,
-//                 title: o.title,
-//                 snippet: o.snippet,
-//                 img: (((o.pagemap || {}).cse_image || [])[0] || {}).src,
-//             })),
-//         };
-
-//         // console.log(resultData);
-
-//         res.status(200).send(resultData);
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).send(err.message || 'Internal Server Error');
-//     }
-// });
-
 router.get('/getArticles', async (req, res, next) => {
+
+    // const authorizationHeader = req.headers.authorization || null;
+  
+    // if (!authorizationHeader) {
+    //   console.error('Access token is missing.');
+    //   res.status(401).json({ error: 'Unauthorized' });
+    //   return;
+    // }
+
+    // console.log(authorizationHeader);
+
+    // const config = {
+    //     headers: {
+    //         'Authorization': authorizationHeader
+    //     }
+    // };
+
     try {
+
         const { q } = req.query;
 
         const response = await axios.get('https://customsearch.googleapis.com/customsearch/v1', {
@@ -100,6 +53,7 @@ router.get('/getArticles', async (req, res, next) => {
                 start: 0,
                 num: 10,
             },
+            // config
         });
 
         const { data } = response;
@@ -125,15 +79,11 @@ router.get('/getArticles', async (req, res, next) => {
             })),
         };
 
+        // console.log(resultData);
+
         res.status(200).send(resultData);
     } catch (err) {
-        console.error('Error:', err.message);
-
-        // Print the error details in the data field
-        if (err.response && err.response.data) {
-            console.error('Error Data:', err.response.data);
-        }
-
+        console.error(err);
         res.status(500).send(err.message || 'Internal Server Error');
     }
 });
