6df50d03447f6b9f551838be798ea9c6dce5fc55
SB379 <sidbalaji03@gmail.com>
2024-01-02 15:02:08 -0500

    slight error handling

--- routes/api/search.js
+++ routes/api/search.js
@@ -23,26 +23,73 @@ const openai = new OpenAI({
 
 router.get('/test', (req, res) => res.send("search route testing"));
 
-router.get('/getArticles', async (req, res, next) => {
+// router.get('/getArticles', async (req, res, next) => {
 
-    // const authorizationHeader = req.headers.authorization || null;
+//     // const authorizationHeader = req.headers.authorization || null;
   
-    // if (!authorizationHeader) {
-    //   console.error('Access token is missing.');
-    //   res.status(401).json({ error: 'Unauthorized' });
-    //   return;
-    // }
+//     // if (!authorizationHeader) {
+//     //   console.error('Access token is missing.');
+//     //   res.status(401).json({ error: 'Unauthorized' });
+//     //   return;
+//     // }
 
-    // console.log(authorizationHeader);
+//     // console.log(authorizationHeader);
 
-    // const config = {
-    //     headers: {
-    //         'Authorization': authorizationHeader
-    //     }
-    // };
+//     // const config = {
+//     //     headers: {
+//     //         'Authorization': authorizationHeader
+//     //     }
+//     // };
 
+//     try {
+
+//         const { q } = req.query;
+
+//         const response = await axios.get('https://customsearch.googleapis.com/customsearch/v1', {
+//             params: {
+//                 key: process.env.CUSTOM_SEARCH_KEY,
+//                 cx: process.env.ENGINE_KEY,
+//                 q,
+//                 start: 0,
+//                 num: 10,
+//             },
+//             // config
+//         });
+
+//         const { data } = response;
+//         const { queries, items, searchInformation } = data;
+
+//         const page = (queries.request || [])[0] || {};
+//         const previousPage = (queries.previousPage || [])[0] || {};
+//         const nextPage = (queries.nextPage || [])[0] || {};
+
+//         const resultData = {
+//             q,
+//             totalResults: page.totalResults,
+//             count: page.count,
+//             startIndex: page.startIndex,
+//             nextPage: nextPage.startIndex,
+//             previousPage: previousPage.startIndex,
+//             time: searchInformation.searchTime,
+//             items: items.map(o => ({
+//                 link: o.link,
+//                 title: o.title,
+//                 snippet: o.snippet,
+//                 img: (((o.pagemap || {}).cse_image || [])[0] || {}).src,
+//             })),
+//         };
+
+//         // console.log(resultData);
+
+//         res.status(200).send(resultData);
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).send(err.message || 'Internal Server Error');
+//     }
+// });
+
+router.get('/getArticles', async (req, res, next) => {
     try {
-
         const { q } = req.query;
 
         const response = await axios.get('https://customsearch.googleapis.com/customsearch/v1', {
@@ -53,7 +100,6 @@ router.get('/getArticles', async (req, res, next) => {
                 start: 0,
                 num: 10,
             },
-            // config
         });
 
         const { data } = response;
@@ -79,14 +125,19 @@ router.get('/getArticles', async (req, res, next) => {
             })),
         };
 
-        // console.log(resultData);
-
         res.status(200).send(resultData);
     } catch (err) {
-        console.error(err);
+        console.error('Error:', err.message);
+
+        // Print the error details in the data field
+        if (err.response && err.response.data) {
+            console.error('Error Data:', err.response.data);
+        }
+
         res.status(500).send(err.message || 'Internal Server Error');
     }
 });
+
 
 router.get('/factCheck', async (req, res) => {
 
