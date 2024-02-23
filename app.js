b5cc33e6e15ba6d6cf8c6a41cc2956fe43c522f2
SB379 <sidbalaji03@gmail.com>
2024-02-23 18:08:34 -0500

    new product, rate limiting, safety features

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
 
 const search = require("./routes/api/search");
 const feed = require("./routes/api/feed");
 
 const app = express();
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: 'Too many requests from this IP, please try again later.'
+  });
 
 dotenv.config();
 
@@ -16,10 +23,12 @@ app.use(express.json({extended: false}));
 
 app.use(bodyParser.json());
 
+app.use(limiter);
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.use('/api/search', search);
-// app.use('/api/feed', feed);
+// app.use('/api/search', search);
+app.use('/api/feed', feed);
 
 
 const port = process.env.PORT || 8082;
--- routes/api/feed.js
+++ routes/api/feed.js
@@ -1,143 +1,138 @@
-// const express = require('express');
-// const router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-// require('dotenv').config();
-// const axios = require('axios');
-// const cheerio = require('cheerio');
+require('dotenv').config();
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-// const langImport = require('@langchain/openai')
-// const langPromptImport = require('@langchain/core/prompts');
-// const langSplitterImport = require('langchain/text_splitter');
-// const langVectorImport = require('langchain/vectorstores/memory');
-// const langCombineImport = require('langchain/chains/combine_documents');
-// const langRetrieverImport = require('langchain/chains/retrieval')
-// const langDocumentImport = require('@langchain/core/documents');
+const langImport = require('@langchain/openai')
+const langPromptImport = require('@langchain/core/prompts');
+const langSplitterImport = require('langchain/text_splitter');
+const langVectorImport = require('langchain/vectorstores/memory');
+const langCombineImport = require('langchain/chains/combine_documents');
+const langRetrieverImport = require('langchain/chains/retrieval')
+const langDocumentImport = require('@langchain/core/documents');
 
 
-// const chatModel = new langImport.ChatOpenAI({
-//   openAIApiKey: process.env.GPT_KEY,
-// });
+const chatModel = new langImport.ChatOpenAI({
+  openAIApiKey: process.env.GPT_KEY,
+});
 
-// const { OpenAI } = require("openai");
-// const { createClient } = require('@supabase/supabase-js');
+const { OpenAI } = require("openai");
+const { createClient } = require('@supabase/supabase-js');
 
-// const openai = new OpenAI({
-//     organization: process.env.GPT_ORG,
-//     apiKey: process.env.GPT_KEY,
-// });
+const openai = new OpenAI({
+    organization: process.env.GPT_ORG,
+    apiKey: process.env.GPT_KEY,
+});
 
-// const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
-// async function getLLMOutput(input, question)
-// {
-//   // batchText = JSON.stringify(input);
-//   batchText = input;
+async function getLLMOutput(input, question)
+{
+  // batchText = JSON.stringify(input);
+  batchText = input;
 
-//   const splitter = new langSplitterImport.RecursiveCharacterTextSplitter();
+  const splitter = new langSplitterImport.RecursiveCharacterTextSplitter();
 
-//   const docOutput = await splitter.splitDocuments([
-//     new langDocumentImport.Document({ pageContent: batchText }),
-//   ]);
+  const docOutput = await splitter.splitDocuments([
+    new langDocumentImport.Document({ pageContent: batchText }),
+  ]);
 
-//   const prompt = langPromptImport.ChatPromptTemplate.fromTemplate(`Answer the following question based only on the provided context:
+  const prompt = langPromptImport.ChatPromptTemplate.fromTemplate(`Answer the following question based only on the provided context:
 
-//   <context>
-//   {context}
-//   </context>
+  <context>
+  {context}
+  </context>
 
-//   Question: {input}`);
+  Question: {input}`);
 
-//   const documentChain = await langCombineImport.createStuffDocumentsChain({
-//     llm: chatModel,
-//     prompt,
-//   });
+  const documentChain = await langCombineImport.createStuffDocumentsChain({
+    llm: chatModel,
+    prompt,
+  });
 
-//   const embeddings = new langImport.OpenAIEmbeddings({
-//     openAIApiKey: process.env.GPT_KEY,
-//   });
+  const embeddings = new langImport.OpenAIEmbeddings({
+    openAIApiKey: process.env.GPT_KEY,
+  });
 
-//   const vectorstore = await langVectorImport.MemoryVectorStore.fromDocuments(
-//     docOutput,
-//     embeddings,
-//   )
+  const vectorstore = await langVectorImport.MemoryVectorStore.fromDocuments(
+    docOutput,
+    embeddings,
+  )
 
-//   const retriever = vectorstore.asRetriever();
+  const retriever = vectorstore.asRetriever();
 
-//   const retrievalChain = await langRetrieverImport.createRetrievalChain({
-//     combineDocsChain: documentChain,
-//     retriever,
-//   })
+  const retrievalChain = await langRetrieverImport.createRetrievalChain({
+    combineDocsChain: documentChain,
+    retriever,
+  })
 
-//   const test = await retrievalChain.invoke({
-//     input : question
-//   });
+  const test = await retrievalChain.invoke({
+    input : question
+  });
 
-//   return test.answer
+  return test.answer
 
-// }
+}
 
-// async function getFramerOutput(url) {
+async function getFramerOutput(url) {
 
-//     try {
-//         // Fetch HTML content from the provided URL
-//         const response = await axios.get(url);
-//         const html = response.data;
+    try {
+        // Fetch HTML content from the provided URL
+        const response = await axios.get(url);
+        const html = response.data;
 
-//         // Load HTML into Cheerio for easy DOM manipulation
-//         const $ = cheerio.load(html);
+        // Load HTML into Cheerio for easy DOM manipulation
+        const $ = cheerio.load(html);
 
-//         // Extract relevant content from the Framer website
-//         const framerContent = $('body').text(); // Modify this to target specific elements if needed
+        // Extract relevant content from the Framer website
+        const framerContent = $('body').text(); // Modify this to target specific elements if needed
 
-//         return framerContent;
+        return framerContent;
 
-//     } catch (error) {
-//         console.error("Error in getting Framer output:", error);
-//         throw error;
-//     }
-// }
+    } catch (error) {
+        console.error("Error in getting Framer output:", error);
+        throw error;
+    }
+}
 
-// router.get('/test', (req, res) => res.send("feed route testing"));
+router.get('/test', (req, res) => res.send("feed route testing"));
 
-// router.get('/generateContent', async (req, res) => {
+router.get('/generateContent', async (req, res) => {
 
-//     // if(req.query.token === "" || req.query.token === null)
-//     // {
-//     //     res.status(500).send("Internal Server Error")
-//     // }
+    try {
+        // Check if the Authorization header is present
+        if (!req.headers.authorization) {
+            return res.status(401).send("Unauthorized: No token provided");
+        }
 
-//     try {
-//         const content = req.query.content;
-//         const framer = req.query.framer;
+        const token = req.headers.authorization.split(' ')[1];
 
-//         question = "You are given a written text about a new project. Give me a summary about the input text as if you are the organization itself. Be as specific as possible in four sentences maximum."
+         // Verify token with Supabase
+         const { data: { user } } = await supabase.auth.getUser(token);
 
-//         if (!content && !framer) {
-//             return res.status(400).send("Both 'content' and 'framer' parameters are missing.");
-//         }
+         if (!user) {
+            return res.status(401).send("Unauthorized: Invalid token");
+         }
 
-//         if (!content) {
-//             const framerContent = await getFramerOutput(framer);
-//             const output = await getLLMOutput(framerContent, question);
-//             return res.send(output);
-//         }
+        story = req.headers.content;
 
-//         if (!framer) {
-//             const output = await getLLMOutput(content, question);
-//             return res.send(output);
-//         }
+        question = "You are given a small blurb about a new tech project that is in its infancy. Give me a tweet in 200 characters or less as if you are the organization launching it. Invoke emotions and tell a masterfully crafted story with it while making it grounded and real. You are allowed to use hashtags that might apply as well."
 
-//         const framerContent = await getFramerOutput(framer);
-//         const batchText = content + framerContent;
-//         const output = await getLLMOutput(batchText, question);
-//         return res.send(output);
+        let output = await getLLMOutput(story, question);
 
-//     } catch (error) {
-//         console.error("Error in generating content:", error);
-//         res.status(500).send("Internal Server Error");
-//     }
-// });
+        output = `${output} Generated with NewsAIfy.com`;
+
+        res.send(output);
+
+    } catch (error)
+    {
+        console.error("Error in generating content:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
 
 
-// module.exports = router;
+module.exports = router;
